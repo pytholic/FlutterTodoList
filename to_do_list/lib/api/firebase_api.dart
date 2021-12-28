96ca5c27507cd44f792fb3696f8c7439129f5783
rajahaseeb147 <rajahaseeb147@gmail.com>
2021-12-28 09:51:10 +0900

    modified api

--- to_do_list/lib/api/firebase_api.dart
+++ to_do_list/lib/api/firebase_api.dart
@@ -2,9 +2,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:to_do_list/model/todo.dart';
 import 'package:to_do_list/utils.dart';
 
+var db = "todo_haseeb"; // database name
+
 class FirebaseApi {
   static Future<String> createTodo(Todo todo) async {
-    final docTodo = FirebaseFirestore.instance.collection("todo_haseeb").doc();
+    final docTodo = FirebaseFirestore.instance.collection(db).doc();
 
     todo.id = docTodo.id;
     await docTodo.set(todo.toJson());
@@ -13,21 +15,19 @@ class FirebaseApi {
   }
 
   static Stream<List<Todo>> readTodos() => FirebaseFirestore.instance
-      .collection("todo_haseeb")
+      .collection(db)
       .orderBy(TodoField.createdTime, descending: true)
       .snapshots()
       .transform(Utils.transformer(Todo.fromJson));
 
   static Future updateTodo(Todo todo) async {
-    final docTodo =
-        FirebaseFirestore.instance.collection("todo_haseeb").doc(todo.id);
+    final docTodo = FirebaseFirestore.instance.collection(db).doc(todo.id);
 
     await docTodo.update(todo.toJson());
   }
 
   static Future deleteTodo(Todo todo) async {
-    final docTodo =
-        FirebaseFirestore.instance.collection("todo_haseeb").doc(todo.id);
+    final docTodo = FirebaseFirestore.instance.collection(db).doc(todo.id);
 
     await docTodo.delete();
   }
